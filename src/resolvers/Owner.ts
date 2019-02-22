2f7966bac011eac8adb7c510d3e3198e1f9fe60e
Jordan Meta <irnawatyully@gmail.com>
2019-02-23 00:08:34 +0800

    Owner.registerAdmin

--- src/resolvers/Owner.ts
+++ src/resolvers/Owner.ts
@@ -5,6 +5,7 @@ export const OwnerResolver = {
   Mutation: {
     ownerCreateCabang: async (_, { payload }, { models: { Owner } }) => Owner.createCabang(payload),
     ownerUpdateCabang: async (_, { id, payload }, { models: { Owner } }) => Owner.updateCabang(id, payload),
-    ownerRemoveCabang: async (_, { id }, { models: { Owner } }) => Owner.removeCabang(id)
+    ownerRemoveCabang: async (_, { id }, { models: { Owner } }) => Owner.removeCabang(id),
+    ownerRegisterAdmin: async (_, { payload }, { models: { Owner } }) => Owner.registerAdmin(payload)
   }
 }
