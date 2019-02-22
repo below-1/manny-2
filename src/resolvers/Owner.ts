d63935676917f8e88e1874838ae11f6e72ddb7ba
Jordan Meta <irnawatyully@gmail.com>
2019-02-22 23:02:07 +0800

    Add authentication to OwnerModel

--- src/resolvers/Owner.ts
+++ src/resolvers/Owner.ts
@@ -4,6 +4,7 @@ export const OwnerResolver = {
   },
   Mutation: {
     ownerCreateCabang: async (_, { payload }, { models: { Owner } }) => Owner.createCabang(payload),
-    ownerUpdateCabang: async (_, { id, payload }, { models: { Owner } }) => Owner.updateCabang(id, payload)
+    ownerUpdateCabang: async (_, { id, payload }, { models: { Owner } }) => Owner.updateCabang(id, payload),
+    ownerRemoveCabang: async (_, { id }, { models: { Owner } }) => Owner.removeCabang(id)
   }
 }
--- src/services/Owner.ts
+++ src/services/Owner.ts
@@ -5,9 +5,16 @@ import {
 
 export class OwnerModel {
 
-  public constructor (private box: Box, private owner: User) {}
+  public constructor (private box: Box, private owner: User) {
+    if (!this.owner || this.owner.kategori != 'owner') {
+      this.owner = null
+    }
+  }
 
   public async createCabang(payload) {
+    if (!this.owner) {
+      throw new Error("Owner is undefiend")
+    }
     let cabang = this.box.repo.cabang.create({
       ...payload
     })
@@ -15,6 +22,9 @@ export class OwnerModel {
   }
 
   public async updateCabang(id, payload) {
+    if (!this.owner) {
+      throw new Error("Owner is undefiend")
+    }
     let cabang = await this.box.repo.cabang.findOne(id)
     cabang.nama = payload.nama
     cabang.alamat = payload.alamat
@@ -22,11 +32,17 @@ export class OwnerModel {
   }
 
   public async removeCabang(id) {
+    if (!this.owner) {
+      throw new Error("Owner is undefiend")
+    }
     await this.box.repo.cabang.delete(id)
     return id
   }
 
   public async registerAdmin(payload) {
+    if (!this.owner) {
+      throw new Error("Owner is undefiend")
+    }
     let user = this.box.repo.user.create({
       nama: payload.nama,
       username: payload.username,
